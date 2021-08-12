113d1313be7f0f7395cf53d1acc31fff77787469
Aime Brues <72033556+SHYAKA-Aime@users.noreply.github.com>
2021-08-12 17:53:53 +0200

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,33 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Declare function and define it
-
-int checkNumber(int n){
-
-    int r,rev=0;
-
-    while(n>0){
-        r=n%10;
-        rev=rev*10+r;
-        n=n/10;
+bool isPalindrome(int x) {
+    //complete the function
+    int y=x,z=0;
+    while(x!=0)
+    {
+        z=z*10+x%10;
+        x=x/10;
+        
     }
-    return rev;
+    if(y==z)
+    return(true);
+    return(false);
 }
 
-int main(){
-
-    int n,num;
-
-    cout<<"Enter a number:";
-    cin>>n;
-
-    num=checkNumber(n);
-    if(num==n){
-        cout<<n<<" is palindrome number";
-    }else{
-        cout<<n<<" is not palindrome number";
+int main() {
+    int n;
+    cout<<"\nEnter number to check: ";
+    cin >>n;
+    
+    if(isPalindrome(n)) {
+        cout <<n<<" is a palindrome number";
     }
-
+    else {
+        cout << n<<" is NOT a palindrome number";
+    }
     return 0;
 }
